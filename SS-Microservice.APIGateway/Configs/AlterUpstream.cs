091a9d169d304182ce0dae86a6d3f75b62b91bdd
MinhSon <nguyenminhson102002@gmail.com>
2023-07-29 11:28:16 +0700

    add config for Ocelot Gateway

---
+++ SS-Microservice.APIGateway/Configs/AlterUpstream.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SS_Microservice.APIGateway.Configs
+{
+    public class AlterUpstream
+    {
+        public static string AlterUpstreamSwaggerJson(HttpContext context, string swaggerJson)
+        {
+            var swagger = JObject.Parse(swaggerJson);
+            // ... alter upstream json
+            return swagger.ToString(Formatting.Indented);
+        }
+    }
+}
--- SS-Microservice.APIGateway/Program.cs
+++ SS-Microservice.APIGateway/Program.cs
@@ -1,7 +1,104 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Cache.CacheManager;
+using Ocelot.Provider.Polly;
+using Microsoft.OpenApi.Models;
+using System.Configuration;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.Values;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var routes = "Routes";
 
+var configuration = builder.Configuration;
+
+// specific multiple ocelot file in Routes folder
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = routes;
+});
+
+//add ocelot with consul
+builder.Services.AddOcelot(builder.Configuration)
+    .AddConsul()
+    .AddCacheManager(x =>
+    {
+        x.WithDictionaryHandle();
+    })
+    .AddPolly();
+
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddOcelot(routes, builder.Environment)
+    .AddEnvironmentVariables();
+
+builder.Services.AddSwaggerGen(s =>
+{
+    s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Description = @"JWT authorization header using the Bearer sheme. \r\n\r\n
+                        Enter 'Bearer' [space] and then your token in the text input below.
+                        \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    s.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference()
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+string issuer = configuration.GetValue<string>("Tokens:Issuer");
+string signingKey = configuration.GetValue<string>("Tokens:Key");
+byte[] signingKeyBytes = Encoding.UTF8.GetBytes(signingKey);
+builder.Services
+    .AddAuthentication(opts =>
+    {
+        opts.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        opts.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(opts =>
+    {
+        opts.RequireHttpsMetadata = false;
+        opts.SaveToken = true;
+        opts.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = issuer,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ClockSkew = TimeSpan.Zero,
+            IssuerSigningKey = new SymmetricSecurityKey(signingKeyBytes)
+        };
+    });
+
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -13,12 +110,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = SS_Microservice.APIGateway.Configs.AlterUpstream.AlterUpstreamSwaggerJson;
+}).UseOcelot().Wait();
 
 app.MapControllers();
 
